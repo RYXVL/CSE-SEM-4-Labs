fcfeb56346c8e103815c4ecd295a98657e8ca339
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-03-24 16:14:24 +0530

    added opcount O(n^2)

--- DAA/Week-2/bubble_sort.c
+++ DAA/Week-2/bubble_sort.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void bubbleSort(int *arr, int size) {
+    int opcount = 0;
     int flag = 1;
     int temp;
-    while(flag!=0) {
+    while(flag != 0) {
         flag = 0;
         for(int i=0; i<size-1; i++) {
-            
+            opcount++;
             if(arr[i]>arr[i+1]) {
                 temp = arr[i];
                 arr[i] = arr[i+1];
@@ -15,18 +17,27 @@ void bubbleSort(int *arr, int size) {
             }
         }
     }
-    for(int i=0 ;i<size; i++)
-        printf("%d ", arr[i]);
+    printf(" Opcount: %d\n", opcount);
 }
 
 int main() {
-    int size_of_array;
-    printf("Enter the size of the array: ");
-    scanf("%d", &size_of_array);
-    int arr[size_of_array];
-    printf("Enter the elements of the array:\n");
-    for(int i=0; i<size_of_array; i++)
-        scanf("%d", arr+i);
-    bubbleSort(arr, size_of_array);
+    int size_of_array = 20;
+    for(int i=0; i<20; i++) {
+        int *arr = (int*)calloc(i+1, sizeof(int));
+        for(int j=0; j<=i; j++)
+            arr[j]=j+1;
+        for(int j=0 ;j<=i; j++)
+            printf("%d ", arr[j]);
+        bubbleSort(arr, i+1);
+    }
+    printf("\n");
+    for(int i=19; i>=0; i--) {
+        int *arr = (int*)calloc(20-i, sizeof(int));
+        for(int j=20; j>i; j--)
+            arr[20-j]=j;
+        for(int j=20 ;j>i; j--)
+            printf("%d ", arr[20-j]);
+        bubbleSort(arr, 20-i);
+    }
     return 0;
 }
