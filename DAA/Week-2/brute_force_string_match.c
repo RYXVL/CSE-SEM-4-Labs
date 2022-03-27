8f4549a814cb38390cc21f312f54c3c8f7cfddc7
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-03-27 08:32:00 +0530

    code without automated input

--- DAA/Week-2/brute_force_string_match.c
+++ DAA/Week-2/brute_force_string_match.c
@@ -1,13 +1,8 @@
 #include<stdio.h>
 
 void searchString(char *main_string, int size_of_main_string, char *search_string, int size_of_search_string) {
-    // printf("%s\n",  main_string);
-    // printf("%s\n", search_string);
-    // printf("%d\n",  size_of_main_string);
-    // printf("%d\n", size_of_search_string);
     int flag = 0;
     for(int i=0; i<size_of_main_string; i++) {
-        // flag = 0;
         if(main_string[i]==search_string[flag]) {
             flag++;
             continue;
@@ -37,10 +32,7 @@ int main() {
     scanf("%s", main_string);
     printf("Enter the string to be searched for: ");
     fflush(stdin);
-    scanf("%s ", search_string);
-
-    // printf("%s\n",  main_string);
-    // printf("%s\n", search_string);
+    scanf("%s", search_string);
 
     searchString(main_string, size_of_main_string, search_string, size_of_search_string);
 
