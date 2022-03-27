de8ea2116544dced177b5b5ffb7936aea349323f
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-03-27 11:05:55 +0530

    added automated input

--- DAA/Week-2/brute_force_string_match.c
+++ DAA/Week-2/brute_force_string_match.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 void searchString(char *main_string, int size_of_main_string, char *search_string, int size_of_search_string) {
     int flag = 0;
@@ -7,34 +8,35 @@ void searchString(char *main_string, int size_of_main_string, char *search_strin
             flag++;
             continue;
         }
-        if(main_string[i]!=search_string[flag]) flag=0;
+        flag=0;
     }
     if(flag==size_of_search_string)
-        printf("Substring found!");
+        printf("Substring found!\n");
     else
-        printf("Substring not found!");
+        printf("Substring not found!\n");
 }
 
 int main() {
-    int size_of_main_string;
-    int size_of_search_string;
-
-    printf("Enter the size of the main string: ");
-    scanf("%d", &size_of_main_string);
-    printf("Enter the size of the string to be searched for: ");
-    scanf("%d", &size_of_search_string);
-
-    char main_string[size_of_main_string];
-    char search_string[size_of_search_string];
-
-    printf("Enter the main string: ");
-    fflush(stdin);
-    scanf("%s", main_string);
-    printf("Enter the string to be searched for: ");
-    fflush(stdin);
-    scanf("%s", search_string);
-
-    searchString(main_string, size_of_main_string, search_string, size_of_search_string);
+    
+    char main_string[20] = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+    
+    for(int i=0; i<20; i++) {
+        if(i==0) {
+            char* search_string = (char*)calloc(i+1, sizeof(char));
+            search_string[i] = 'b';
+            searchString(main_string, 20 , search_string, i+1);
+            continue;
+        }
+        char* search_string = (char*)calloc(i+1, sizeof(char));
+        for(int j=0; j<i+1; j++) {
+            if(j==i) {
+                search_string[j]='b';
+                continue;
+            }
+            search_string[j] = 'a';
+        }
+        searchString(main_string, 20 , search_string, i+1); 
+    }
 
     return 0;
 }
