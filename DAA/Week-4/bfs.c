0cd6503a4041b16ed1db88a9d23d313ab66650bd
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-04-11 14:18:05 +0530

    final

--- DAA/Week-4/bfs.c
+++ DAA/Week-4/bfs.c
@@ -1,40 +1,41 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int number_of_vertices;
+int queue[100];
+int front = -1;
+int rear = -1;
 
-void dfsAtVertex(int vertex, int adjacency_matrix[number_of_vertices][number_of_vertices], int visited[number_of_vertices]) {
+void bfsAtVertex(int vertex, int adjacency_matrix[number_of_vertices][number_of_vertices], int visited[number_of_vertices]) {
 	printf("%c ", vertex+65);
-	// pushStack[++tos_pushStack] = vertex;
 	visited[vertex]=1;
-	// int flag = 0;
-	// for(int i=0; i<number_of_vertices; i++) {
-	// 	if(i!=vertex && !(visited[i]) && adjacency_matrix[vertex][i]==1)
-	// 		flag=1;
-	// }
-	for(int i=0; i<number_of_vertices; i++) {
-		if(i!=vertex && !(visited[i]) && adjacency_matrix[vertex][i]==1)
-			dfsAtVertex(i, adjacency_matrix, visited);
-	}
-	// if(flag==0) popStack[++tos_popStack] = vertex;
+	for(int i=0; i<number_of_vertices; i++)
+		if(i!=vertex && !visited[i] && adjacency_matrix[vertex][i]==1)
+			queue[++rear] = i;
 }
 
-void dfs(int adjacency_matrix[number_of_vertices][number_of_vertices], int *visited) {
-	for(int i=0; i<number_of_vertices; i++)
-		if(!visited[i])
-			dfsAtVertex(i, adjacency_matrix, visited);
+void bfs(int adjacency_matrix[number_of_vertices][number_of_vertices], int *visited) {
+	int num;
+	queue[++rear]=0;
+	while(front!=rear) {
+		num = queue[++front];
+		if(!visited[num])
+			bfsAtVertex(num, adjacency_matrix, visited);
+	}
 }
 
 int main() {
 	printf("Enter the number of vertices: ");
 	scanf("%d", &number_of_vertices);
+
 	int adjacency_matrix[number_of_vertices][number_of_vertices];
 	for(int i=0; i<number_of_vertices; i++)
 		for(int j=0; j<number_of_vertices; j++)
 			adjacency_matrix[i][j]=0;
+
 	int visited[number_of_vertices];
 	for(int i=0; i<number_of_vertices; i++)
 		visited[i]=0;
+
 	for(int i=0; i<number_of_vertices-1; i++) {
 		for(int j=i+1; j<number_of_vertices; j++) {
 			printf("Is %c connected to %c?: ", i+65, j+65);
@@ -42,16 +43,7 @@ int main() {
 			adjacency_matrix[j][i]=adjacency_matrix[i][j];
 		}
 	}
-	dfs(adjacency_matrix, visited);
-	printf("Push Order: ");
-	for(int i=0; i<=tos_pushStack; i++)
-		printf("%c\n", pushStack[i]+65);
-	printf("\n");
-	printf("Pop Order: ");
-	for(int i=0; i<=tos_popStack; i++)
-		printf("%c\n", popStack[i]+65);
-	printf("\n");
+
+	bfs(adjacency_matrix, visited);
 	return 0;
 }
-
-// 1 0 0 1 1 0
