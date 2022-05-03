f6840d7fc7a1ef310273243c536762881b48545a
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-05-03 14:47:44 +0530

    final with opcount and automated input

--- DAA/Week-6/quicksort.c
+++ DAA/Week-6/quicksort.c
@@ -1,59 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int opcount = 0;
-void swap(int *num1, int *num2) {
-	int temp = *num1;
-	*num1 = *num2;
-	*num2 = temp;
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int partition(int arr[], int size, int low, int high) {
-	int pivot = arr[high];
-	int i = low - 1;
-	for(int j = low; j<=high - 1; j++){
-		opcount++;
-        if(arr[j]<pivot) {
-			i++;
-			swap(&arr[i], &arr[j]);
-		}
+int partition(int arr[], int lb, int ub) {
+    int pivot, start, end;
+    pivot = arr[lb];
+    start = lb;
+    end = ub + 1;
+    do {
+        do {
+            opcount++;
+            start++;
+        } while (arr[start] < pivot);
+        do {
+            opcount++;
+            end--;
+        } while (arr[end] > pivot);
+        if (start < end)
+            swap(&arr[start], &arr[end]);
+    } while (start < end);
+    swap(&arr[lb], &arr[end]);
+    return end;
+}
+
+void quickSort(int arr[], int lb, int ub) {
+    int loc;
+    if (lb < ub) {
+        loc = partition(arr, lb, ub);
+        quickSort(arr, lb, loc - 1);
+        quickSort(arr, loc + 1, ub);
     }
-	swap(&arr[i+1], &arr[high]);
-	return (i+1);
-}
-
-void quickSort(int arr[], int size, int low, int high) {
-	if(low<high) {
-		int pi = partition(arr, size, low, high);
-		quickSort(arr, size, low, pi - 1);
-		quickSort(arr, size, pi + 1, high);
-	}
 }
 
 int main() {
     for(int i=0; i<20; i++) {
         opcount = 0;
         int arr[i+1];
-        // int arr[i+1];
-        // int temp[i+1];
         for(int j=0; j<i+1; j++)
             arr[j] = j+1;
-        quickSort(arr, i+1, 0, i+1);
+        quickSort(arr, 0, i);
         for(int j=0; j<i+1; j++)
             printf("%d ", arr[j]);
         printf("Opcount: %d", opcount);
         printf("\n");
     }
-	// int size;
-	// printf("Enter the size of the array: ");
-	// scanf("%d", &size);
-	// int arr[size];
-	// printf("Enter the elements of the array: ");
-	// for(int i=0; i<size; i++)
-	// 	scanf("%d", &arr[i]);
-	// quickSort(arr, size, 0, size - 1);
-	// printf("The sorted array is: ");
-	// for(int i=0; i<size; i++)
-	// 	printf("%d ", arr[i]);
-    //     printf("Opcount: %d", opcount);
-	return 0;
+    return 0;
 }
