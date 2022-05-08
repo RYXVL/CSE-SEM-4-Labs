54a6a2c7e7484f74797bb94237f933bd9dde0b67
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-05-08 15:48:21 +0530

    Update key_8bit_ring_counter.c

--- ES/Week-6/key_8bit_ring_counter.c
+++ ES/Week-6/key_8bit_ring_counter.c
@@ -1,37 +1,23 @@
 #include <LPC17xx.h>
-
-int main(void)
-{
+int main(void) {
 	unsigned int j;
 	unsigned long LED;
-
-	//Configure Port0 PINS P0.4-P0.11 as GPIO function
-	LPC_PINCON->PINSEL0 &= 0xFF0000FF;
-
-	//Configure P0.4-P0.11 as output port
-	LPC_GPIO0->FIODIR |= 0x00000FF0;
-
-	LPC_PINCON->PINSEL4 &= 0xFCFFFFFF;
-	LPC_GPIO2->FIODIR &= 0xFFFFEFFF;   //P2.12 as input port
-	while(1)
-	{
-		if(LPC_GPIO2->FIOPIN & 1<<12) //switch not pressed
-		{
-			for(LED=128;LED>=1;LED/=2) //Down Counter
-			{
+    LPC_PINCON->PINSEL0 &= 0xFF0000FF;
+    LPC_GPIO0->FIODIR |= 0x00000FF0;
+    LPC_PINCON->PINSEL4 &= 0xFCFFFFFF;
+	LPC_GPIO2->FIODIR &= 0xFFFFEFFF;
+	while(1) {
+		if(LPC_GPIO2->FIOPIN & 1<<12) {
+			for(LED=128;LED>=1;LED/=2) {
 				LPC_GPIO0->FIOPIN = LED<<4;
-
-				for(j=0;j<100000;j++);//a random delay
-
-			} // loop for 255 times
-		} else {
-			for(LED=1;LED<=128;LED*=2) //Up counter
-			{
+				for(j=0;j<100000;j++);
+			}
+		} 
+        else {
+			for(LED=1;LED<=128;LED*=2) {
 				LPC_GPIO0->FIOPIN = LED<<4;
-
-				for(j=0;j<100000;j++);//a random delay
-
-			} // loop for 255 times
+				for(j=0;j<100000;j++);
+			}
 		}
 	}
 }
