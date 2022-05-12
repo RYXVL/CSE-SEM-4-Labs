4c77cd7bfbd980ca864dc791527b69e04a8359e2
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-05-12 09:27:17 +0530

    final

--- DAA/Week-7/avl.c
+++ DAA/Week-7/avl.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
  
 typedef struct node* nodeptr;
-typedef struct node{
-    int key;
+typedef struct node {
+    int data;
     nodeptr left;
     nodeptr right;
     int height;
@@ -12,138 +12,121 @@ typedef struct node{
 nodeptr stack[100];
 int tos = -1;
  
-int max(int a, int b){
-    return a>b?a:b;
+int max(int a, int b) {
+    return a > b? a : b;
 }
 
-int height(nodeptr root){
-    if (!root) return 0;
-    return 1+max(height(root->left), height(root->right));
+int height(nodeptr N){
+    if (!N) return 0;
+    return 1 + max(height(N->left), height(N->right));
 }
  
-nodeptr getNode(int key){
+nodeptr getNode(int data){
     nodeptr newnode = (nodeptr)malloc(sizeof(*newnode));
-    newnode->key = key;
+    newnode->data = data;
     newnode->left = newnode->right = NULL;
     newnode->height = 0;
     return newnode;
 }
  
-void rightRotate(nodeptr* root, nodeptr* y)
+nodeptr rightRotate(nodeptr* y)
 {
     nodeptr og = *y;
-    nodeptr x = (*y)->left;
+    nodeptr x = og->left;
     nodeptr T2 = x->right;
-    x->right = *y;
-    (*y)->left = T2;
-    (*y)->height = height(*y);
+    x->right = og;
+    og->left = T2;
+    og->height = height(og);
     x->height = height(x);
-    og = x;
-    nodeptr new = stack[tos--];
-    if(!(new)) *root = x;
-    else {
-        if((new)->left->key = (*y)->key)
-            (new)->left = x;
-        else
-            (new)->right = x;
-    }
+    return x;
 }
 
-void leftRotate(nodeptr* root, nodeptr* x){
+nodeptr leftRotate(nodeptr* x){
     nodeptr og = *x;
-    nodeptr y = (*x)->right;
+    nodeptr y = og->right;
     nodeptr T2 = y->left;
-    y->left = *x;
-    (*x)->right = T2;
-    (*x)->height = height(*x);
+    y->left = og;
+    og->right = T2;
+    og->height = height(og);
     y->height = height(y);
-    og = y;
-    nodeptr new = stack[tos--];
-    if(!(new)) *root = y;
-    else {
-        if((new)->left->key = (*x)->key)
-            (new)->left = y;
-        else
-            (new)->right = y;
-    }
+    return y;
 }
  
-int getBalance(nodeptr root){
-    if (!root) return 0;
-    return height(root->left) - height(root->right);
+int getBalance(nodeptr N){
+    if (!N) return 0;
+    return height(N->left) - height(N->right);
 }
 
 
-void balance(nodeptr* root, int key){
+nodeptr balance(int data, nodeptr *root){
+    nodeptr newRoot = *root;
     while(tos != -1){
         nodeptr node = stack[tos--];
+        nodeptr prev = tos == -1 ? NULL : stack[tos];
         node->height = height(node);
         int balance = getBalance(node);
-        if (balance > 1 && key < node->left->key) rightRotate(root, &node);
-        if (balance < -1 && key > node->right->key) leftRotate(root, &node);
-        if (balance > 1 && key > node->left->key) {
-            leftRotate(root, &node->left);
-            rightRotate(root, &node);
+        if (balance > 1 && data < node->left->data) node = rightRotate(&node);
+        if (balance < -1 && data > node->right->data) node = leftRotate(&node);
+        if (balance > 1 && data > node->left->data) {
+            node->left = leftRotate(&node->left);
+            node = rightRotate(&node);
         }
-        if (balance < -1 && key < node->right->key){
-            rightRotate(root, &node->right);
-            leftRotate(root, &node);
+        if (balance < -1 && data < node->right->data){
+            node->right = rightRotate(&node->right);
+            node = leftRotate(&node);
+        }
+        if(prev){
+            if(prev->data > node->data) prev->left = node;
+            else prev->right = node;
+        }
+        newRoot = node;
     }
-    }
+    return newRoot;
 }
 
-void insert(nodeptr* root, int key){
-    nodeptr curr = *root;
+nodeptr insertIntoBST(nodeptr* node, int data){
+    tos = -1;
+    nodeptr curr = *node;
     while(1){
         if(!curr) {
-            curr = getNode(key);
+            curr = getNode(data);
             break;
         }
         stack[++tos] = curr;
-        if(key > curr->key){
+        if(data > curr->data){
             if(!curr->right) {
-                curr->right = getNode(key);
+                curr->right = getNode(data);
                 break;
             }
             else curr = curr->right;
         } 
-        else if(key < curr->key){ 
+        else if(data < curr->data){ 
             if(!curr->left) {
-                curr->left = getNode(key);
+                curr->left = getNode(data);
                 break;
             }
             else curr = curr->left;
         }
     }
-    balance(root, key);
+    return balance(data, node);
 }
 
-void inOrder(nodeptr root){
-    if(root){
+void inOrder(nodeptr root) {
+    if(root) {
         inOrder(root->left);
-        printf("%d ", root->key);
+        printf("%d ", root->data);
         inOrder(root->right);
     }
 }
  
-int main()
-{
+int main() {
     nodeptr root = getNode(10);
-    insert(&root, 20);
-    insert(&root, 30);
-    insert(&root, 40);
-    insert(&root, 50);
-    insert(&root, 25);
-  /*
-
-            30
-           /  \
-         20   40
-        /  \     \
-       10  25    50
-  */
- 
-  printf("Inorder traversal of the constructed AVL tree is \n");
-  inOrder(root);
-  return 0;
+    root = insertIntoBST(&root, 20);
+    root = insertIntoBST(&root, 30);
+    root = insertIntoBST(&root, 40);
+    root = insertIntoBST(&root, 50);
+    root = insertIntoBST(&root, 25);
+    printf("Preorder Traversal of AVL Tree is : ");
+    inOrder(root);
+    return 0;
 }
