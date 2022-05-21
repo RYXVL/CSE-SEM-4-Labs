d805e0768938b58c456f9c285d48bdabb1d52b16
RYXVL <95095676+RYXVL@users.noreply.github.com>
2022-05-22 00:24:51 +0530

    without opcount

--- DAA/Week-8/heap_top_down.c
+++ DAA/Week-8/heap_top_down.c
@@ -11,55 +11,43 @@ void swap(int *a, int *b) {
 
 void heapify(int* arr, int end) {
 	int flag = 1;
+    int largest;
 	while(flag) {
 		flag = 0;
-		for(int i=0; i<=end/2-1; i++) {
+		for(int i=0; i<=(end-1)/2; i++) {
+            largest = i;
 			opcount+=4;
-			if(2*i+1<=end && arr[i]<arr[2*i+1]) {
-				swap(&arr[i], &arr[2*i+1]);
+			if(2*i+1<=end && arr[largest]<arr[2*i+1]) {
+				largest = 2*i+1;
 				flag = 1;
 			}
-			if(2*i+2<=end && arr[i]<arr[2*i+2]) {
-				swap(&arr[i], &arr[2*i+2]);
+			if(2*i+2<=end && arr[largest]<arr[2*i+2]) {
+				largest = 2*i+2;
 				flag = 1;
 			}
+            if(largest!=i)
+                swap(&arr[i], &arr[largest]);
 		}
 	}
 }
-
-// void heapify(int* arr, int end) {
-// 	int flag = 1;
-// 	while(flag) {
-// 		flag = 0;
-// 		for(int i=0; i<=end/2-1; i++) {
-// 			opcount+=2;
-// 			if(arr[i]<arr[2*i+1]) {
-// 				// opcount++;
-// 				swap(&arr[i], &arr[2*i+1]);
-// 				flag = 1;
-// 			}
-// 			if(arr[i]<arr[2*i+2]) {
-// 				// opcount++;
-// 				swap(&arr[i], &arr[2*i+2]);
-// 				flag = 1;
-// 			}
-// 		}
-// 	}
-// }
 
 int main() {
 	int n;
 	printf("Enter the number of elements in the array: ");
 	scanf("%d", &n);
-	int *arr = (int*)calloc(n, sizeof(int));
+
+	int arr[n];
 	printf("Enter the elements of the array: ");
 	for(int i=0; i<n; i++) {
 		scanf("%d", &arr[i]);
 		heapify(arr, i);
 	}
+
 	printf("The Heap is: ");
 	for(int i=0; i<n; i++)
 		printf("%d ", arr[i]);
+
 	printf("Opcount: %d", opcount);
+
 	return 0;
 }
